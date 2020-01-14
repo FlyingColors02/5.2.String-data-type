145f0a40faae0c6718afa44cfb809bf6f67ce0a0
FlyingColors02 <shraduborse97@gmail.com>
2020-01-14 11:11:32 +0530

    #shraddha borse: string data type

---
+++ main.js
@@ -0,0 +1,17 @@
+/*     DATA TYPE 
+    1. Value Type    --> NUMBER, STRING, BOOLEAN
+    2.Reference Type --> ARRAY, CLASS              */
+
+//string Data Type:
+
+let strValue1; //declaration
+ strValue1="shraddha borse";  //defining string value
+ console.log(strValue1); //shraddha borse
+ console.log(typeof(strValue1)); //string
+
+
+strValue1="10";
+console.log(strValue1);//10
+console.log(typeof(strValue1));// string
+
+//any thing withing double-quotes("") is consider as string
